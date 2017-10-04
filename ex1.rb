19cf80a119fdccd8eb027ae5e2d5e8552e1ffd8c
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-04 19:42:55 -0400

    initialized a trip and created some stops, printed out the aray with all method

--- ex1.rb
+++ ex1.rb
@@ -12,9 +12,11 @@ end
 
 class Trip < Location
 
+  # This will be an array of Location instances:
   @@stops = []
 
   attr_accessor :name
+
   def initiate(name)
     @name = name
   end
@@ -23,13 +25,25 @@ class Trip < Location
     puts "Begin trip"
     @@stops.each { |loaction| puts "I stopped at #{location}"  }
     puts "End of trip"
+
+    #alternate way usin the hint:
+    # @@stops.each_cons(2) do |locations|
+    #  puts "Travelled from #{ locations[0].name} to #{locations[1].name}"
+
   end
 
+ # define a method that lets you add locations to the trips list of destinations
   def self.create(name)
     new_location = Location.new(name)
     @@stops << new_location
     return new_location
   end
-
-
 end
+
+trip = Trip.new("October trip")
+Trip.create("Ottawa")
+Trip.create("Montreal")
+Trip.create("Quebec City")
+Trip.create("Halifax")
+Trip.create("St. John's")
+Trip.all
